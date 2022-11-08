8b3929919a69a9bbf993e7ef90b64e3d0a9fda03
rodrigomd94 <rodrigomd94@gmail.com>
2022-11-08 11:11:35 -0600

    Remove test description

--- components/NftGrid.tsx
+++ components/NftGrid.tsx
@@ -10,7 +10,7 @@ console.log(props)
         <>
         <div className="grid grid-cols-4 gap-2">
                 {props.nfts.map((nft : any, index : Number) => {
-                    return <NftCard key={index} update={props.update} refUnit={nft.refUnit} meta={{...nft.metadata, description:"test"}} />
+                    return <NftCard key={index} update={props.update} refUnit={nft.refUnit} meta={{...nft.metadata}} />
                 })}
             </div>
         </>
